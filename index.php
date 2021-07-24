67e521f0cfc302fa86f48c5a9207c6a8bca4d2f6
John-maura <johntemwaura992@gmail.com>
2021-07-24 15:46:02 +0300

    worked on CRUD aspect

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud','root','');
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-//echo '<pre>';
-//var_dump($products);
-//echo '</pre>';
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="app.css">
-    <title>products CRUD</title>
+    <link href="app.css" rel="stylesheet" />
+    <title>Products CRUD</title>
 </head>
 
 <body>
-    <h1 class="">products CRUD</h1>
+    <h1>Products CRUD</h1>
+
     <p>
-        <a href="create.php" class="btn btn-success">Create Product</a>
+        <a href="create.php" type="button" class="btn btn-sm btn-success">Add Product</a>
     </p>
     <table class="table">
         <thead>
@@ -36,25 +36,33 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
                 <th scope="col">Title</th>
                 <th scope="col">Price</th>
                 <th scope="col">Create Date</th>
-                <th scope="col">Action</th>
+                <th scope="col">Actions</th>
             </tr>
         </thead>
         <tbody>
-        <?php foreach ($products as $i => $product):?>
-            <tr>
-                <th scope="row"><?php $i + 1 ?></th>
-                <td></td>
-                <td><?php echo $product['title']?></td>
-                <td><?php echo $product['price']?></td>
-                <td><?php echo $product['create_date']?></td>
-                <td>
-                   <button type="button" class="btn btn-sm btn-outline-primary">edit</button>
-                    <button type="button" class="btn btn-sm btn-outline-danger">delete</button>
-                </td>
-            </tr>
-        <?php endforeach; ?>
+            <?php foreach ($products as $i => $product) { ?>
+                <tr>
+                    <th scope="row"><?php echo $i + 1 ?></th>
+                    <td>
+                        <?php if ($product['image']) : ?>
+                            <img src="<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>" class="product-img">
+                        <?php endif; ?>
+                    </td>
+                    <td><?php echo $product['title'] ?></td>
+                    <td><?php echo $product['price'] ?></td>
+                    <td><?php echo $product['create_date'] ?></td>
+                    <td>
+                        <a href="update.php?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                        <form method="post" action="delete.php" style="display: inline-block">
+                            <input type="hidden" name="id" value="<?php echo $product['id'] ?>" />
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
+
 </body>
 
 </html>
